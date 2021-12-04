8074d6917838ba81a8b297341a869c701aef2990
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-12-03 22:02:28 -0300

    Implement first visual styles

---
+++ src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,17 @@
+import "./NavigationBar.css";
+
+function NavigationBar() {
+    return (
+        <nav>
+            <ul>
+                <li id="title">quasardev</li>
+                <li>Home</li>
+                <li>Postagens</li>
+            </ul>
+            <ul>
+            </ul>
+        </nav>
+    );
+  }
+  
+  export default NavigationBar;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./styles/global.css";
+import "./styles/imports.css";
 
 ReactDOM.render(
   <React.StrictMode>
